d3e0039cd3a96d5f7b97f95417424840423ab659
Murilo Silva <murilosilvavmt@gmail.com>
2024-04-17 11:01:56 -0300

    test debug log

--- pkg/nib/rnib/rnib.go
+++ pkg/nib/rnib/rnib.go
@@ -62,7 +62,7 @@ func (c *Client) DeleteRsmSliceList(ctx context.Context, nodeID topoapi.ID) erro
 func (c *Client) HasRANFunction(ctx context.Context, nodeID topoapi.ID, oid string) bool {
 	// get E2Nodes aspects
 	e2Node, err := c.GetE2NodeAspects(ctx, nodeID)
-	log.Debug("test HasRANFunc")
+	log.Debug("test HasRANFunc2")
 	if err != nil {
 		log.Warn(err)
 		return false
