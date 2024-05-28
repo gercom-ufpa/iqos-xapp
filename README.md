822bca96ceaf5c469345189445f38816c9fd322c
Murilo Silva <murilosilvavmt@gmail.com>
2024-05-28 18:51:52 -0300

    xApp test

--- README.md
+++ README.md
@@ -4,8 +4,31 @@ Add a paragraph about the application here...
 
 ## Requirements
 
-- Swig v4.1 or newer (for xapp_sdk);
-- libsctp-dev
-- libpcre2-dev
+- Near-RT RIC FlexRIC;
 
 ## Getting Started
+
+### Installing the xApp
+
+After installing FlexRIC, clone the xApp repository with the following command.
+
+```shell
+git clone --recurse-submodules -j8 git@github.com:gercom-ufpa/iqos-xapp.git
+```
+
+Build the xApp.
+
+```shell
+cd iqos-xapp
+mkdir -p build && cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DE2AP_VERSION=E2AP_V3 -DKPM_VERSION=KPM_V3_00 ..
+ninja iqos-xapp
+```
+
+### Running the xApp
+
+After building, the xApp can be run with the following command.
+
+```shell
+./iqos-xapp -c xApp.conf
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <unistd.h>
 
-int main() {
-    std::cout << "Hello, FlexRIC!" << '\n';
-    return 0;
+#include "../libs/flexric/src/xApp/e42_xapp_api.h"
+#include "defer.hpp"
+
+int main(int argc, char *argv[]) {
+    // format args
+    fr_args_t args{init_fr_args(argc, argv)};
+
+    // set FlexRIC IP (just for development)
+    args.ip = {"192.168.122.20"};
+
+    // init xApp
+    init_xapp_api(&args);
+    sleep(1);
+
+    // get E2 Nodes
+    e2_node_arr_xapp_t e2Nodes{e2_nodes_xapp_api()};
+    assert(e2Nodes.len > 0 && "There are no E2 nodes connected!");
+
+    // free memory allocated to E2 Nodes when finished
+    defer(free_e2_node_arr_xapp(&e2Nodes));
+
+    // TODO: print E2 nodes infos
+    std::cout << "There are " << static_cast<unsigned>(e2Nodes.len) << " E2 nodes connected" << '\n';
+
+    // wait until all xApp processes have been completed
+    while (try_stop_xapp_api() == false) {
+        usleep(1000); // 1 ms
+    }
+
+    std::cout << "Hello, FlexRIC :)!" << '\n';
+    return EXIT_SUCCESS;
 }
